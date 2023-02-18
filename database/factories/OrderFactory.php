810c3fcc4a8f5d1b090cfccd1fcb897f48c477e2
Haruki1707 <72423267+Haruki1707@users.noreply.github.com>
2023-02-18 11:31:18 -0600

    Order model -mfs finished

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $subTotal = fake()->randomFloat(2, 5, 500);
+        $shipping = fake()->randomFloat(2, 1, 10);
+        $taxes = fake()->randomFloat(2, 1, 20);
+
+        return [
+            'user_id' => 1,
+            'item_count' => fake()->numberBetween(1, 20),
+            'sub_total' => $subTotal,
+            'shipping_cost' => $shipping,
+            'taxes' => $taxes,
+            'total' => $subTotal + $shipping + $taxes,
+        ];
+    }
+}
---
+++ database/migrations/2023_02_17_033727_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\User::class)->constrained()->cascadeOnUpdate()->restrictOnDelete();
+            $table->integer('item_count');
+            $table->double('sub_total');
+            $table->double('shipping_cost');
+            $table->double('taxes');
+            $table->double('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
