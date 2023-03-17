a236b639577566b876809b6f1f8c43151ebe00db
Alca <karenrivasg17@gmail.com>
2023-03-16 20:02:23 -0600

    Auth layout added

--- resources/js/Pages/Products/CreateEdit.vue
+++ resources/js/Pages/Products/CreateEdit.vue
@@ -1,44 +1,45 @@
 <script setup>
 import { useForm, usePage } from '@inertiajs/vue3';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 
 const props = defineProps(['categories', 'product', 'selected_categories']);
 
 const form = useForm({
-    name:props.product?.name ?? '',
-    description:props.product?.description ?? '',
-    unit_price:props.product?.unit_price ?? 0.00,
-    categories:props.selected_categories ?? [],
-    quantity:props.product?.quantity ?? 0,
+    name: props.product?.name ?? '',
+    description: props.product?.description ?? '',
+    unit_price: props.product?.unit_price ?? 0.00,
+    categories: props.selected_categories ?? [],
+    quantity: props.product?.quantity ?? 0,
 });
 
-const store = ()=>{
+const store = () => {
     form.post(route('products.store'), {
-        onSuccess:()=>{
+        onSuccess: () => {
             form.reset();
             alert(usePage().props.flash.message);
         }
     });
 };
 
-const update = ()=> {
-    form.patch(route('products.update', {id:props.product.id}), {
-        onSuccess:()=>{
+const update = () => {
+    form.patch(route('products.update', { id: props.product.id }), {
+        onSuccess: () => {
             alert(usePage().props.flash.message);
         }
     });
 };
 
-const destroy = ()=> {
-    form.delete(route('products.destroy', {id:props.product.id}), {
-        onSuccess:()=>{
+const destroy = () => {
+    form.delete(route('products.destroy', { id: props.product.id }), {
+        onSuccess: () => {
             alert(usePage().props.flash.message);
         }
     });
 }
 
-const remove = ()=> {
-    form.delete(route('products.remove', {id:props.product.id}), {
-        onSuccess:()=>{
+const remove = () => {
+    form.delete(route('products.remove', { id: props.product.id }), {
+        onSuccess: () => {
             alert(usePage().props.flash.message);
         }
     });
@@ -47,20 +48,32 @@ const remove = ()=> {
 </script>
 
 <template>
-    <form @submit.prevent="(product==null ? store() : update())">
-        <input v-model="form.name" type="text" maxlength="255">
-        <textarea v-model="form.description" maxlength="5000"></textarea>
-        <input v-model="form.unit_price" type="number" step="0.01" min="0.01">
+    <AuthenticatedLayout>
+        <form @submit.prevent="(product == null ? store() : update())">
+            <div>
+                <input v-model="form.name" type="text" maxlength="255">
+            </div>
+            <div>
+                <textarea v-model="form.description" maxlength="5000"></textarea>
+            </div>
+            <div>
+                <input v-model="form.unit_price" type="number" step="0.01" min="0.01">
+            </div>
+            <div>
 
-        <input v-model="form.quantity" type="number" min="0">
-        <select v-model="form.categories" multiple>
-            <option v-for="category in categories" :key="category.id" :value="category.id">
-                {{ category.name }}
-            </option>
-        </select>
+                <input v-model="form.quantity" type="number" min="0">
+            </div>
+            <div>
+                <select v-model="form.categories" multiple>
+                    <option v-for="category in categories" :key="category.id" :value="category.id">
+                        {{ category.name }}
+                    </option>
+                </select>
+            </div>
 
-        <input type="submit">
-    </form>
-    <button @click="destroy" v-if="product!=null">Global Delete</button>
-    <button @click="remove" v-if="product!=null">Delete From This Warehouse</button>
+            <input type="submit">
+        </form>
+        <button @click="destroy" v-if="product != null">Global Delete</button>
+        <button @click="remove" v-if="product != null">Delete From This Warehouse</button>
+    </AuthenticatedLayout>
 </template>
