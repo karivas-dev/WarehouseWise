0a48ba11aa5224b84995d4975fe6a43f6aebc447
Diego Gómez <72423267+Haruki1707@users.noreply.github.com>
2023-03-16 19:20:51 -0600

    Added Role model with mfs

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = [
+            'administrator',
+            'employee'
+        ];
+
+        Role::factory(count($roles))->sequence(fn ($sqn) => ['type' => $roles[$sqn->index]])->create();
+    }
+}
