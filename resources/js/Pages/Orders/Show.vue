7a7965ba84f30cf52e1ab310c5f54df37a6a9087
Yosiak-alv <Josias_romero60@outlook.com>
2023-05-05 19:30:37 -0600

    Show single order for a order

---
+++ resources/js/Pages/Orders/Show.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {Head} from "@inertiajs/vue3";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import SingleOrder from "@/Pages/Orders/SingleOrder.vue";
+
+const props = defineProps(['order']);
+</script>
+
+<template>
+    <Head title="Orders" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl leading-tight">Order</h2>
+        </template>
+
+        <SingleOrder :order="order"/>
+    </AuthenticatedLayout>
+</template>
